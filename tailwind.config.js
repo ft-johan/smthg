02cc0cc30f429e3ab531d3f17b26d0e618e1c6a7
ft-johan <johangeorge201203@gmail.com>
2025-01-04 19:09:53 +0530

    tailwind bug fix#1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  future: {
+    removeDeprecatedGapUtilities: true,
   },
+  purge: ["./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  variants: {},
   plugins: [],
 }
-
