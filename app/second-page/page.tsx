460a8234356143c536e615567c2f4639056a2090
ft-johan <johangeorge201203@gmail.com>
2025-01-06 20:15:44 +0530

    recorrected

--- app/second-page/page.tsx
+++ app/second-page/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-
+import Image from "next/image";
 
 import { useEffect, useState } from "react";
 
@@ -35,15 +35,10 @@ export default function Moonie() {
 
   return (
     <div className="w-full mx-auto rounded-md  h-screen overflow-hidden">
-      <div
-        
-        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
-      >
+      <div className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full">
         <div className="flex flex-col items-center justify-center min-h-screen bg-transparent text-white space-y-6 p-4">
           <h1 className="text-9xl font-bold text-center">Your look alike</h1>
-          
 
-          
           {loading && <p className="text-2xl">Loading your photo...</p>}
 
           {/* Show error if any */}
@@ -53,24 +48,26 @@ export default function Moonie() {
           {moonImage && !loading && (
             <div className="mt-6">
               <p className="text-xl text-center">
-            "Captured by NASA, revealed just for you."
-          </p>
-              <img
+              &quot; Captured by NASA, revealed just for you.&quot;
+              </p>
+              <Image
                 src={moonImage}
                 alt="Moon"
-                className="max-w-96 h-auto border-1 border-white rounded-lg  content-center"
+                width={384}
+                height={300}
               />
+             
               <p className="mt-4 text-lg text-center">
-                "Behold a planetary vision, just for you!"
+              &quot;Behold a planetary vision, just for you!&quot;
               </p>
-              
-              
             </div>
-          )}{moonImage && !loading && (
-         <p className="mt-4 text-sm text-center italic">
-                "Due to sum technical difficulties the sysytem only can render fraction of your beautiii🥲🥲"
-              </p>
-               )}
+          )}
+          {moonImage && !loading && (
+            <p className="mt-4 text-sm text-center italic">
+              &quot; Due to sum technical difficulties the sysytem only can render
+              fraction of your beautiii🥲🥲 &quot;
+            </p>
+          )}
         </div>
       </div>
     </div>
